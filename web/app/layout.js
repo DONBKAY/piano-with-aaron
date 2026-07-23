83d089131b72828964bcc0cbd21cacaed01c6b20
DONBKAY <DONBKAY@users.noreply.github.com>
2026-07-23 10:12:37 +0000

    Update layout.js

--- web/app/layout.js
+++ web/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import WhatsAppButton from "../components/WhatsAppButton";
+import MetaPixel from "../components/MetaPixel";
 
 export const metadata = {
   title: "Piano With Aaron",
@@ -13,14 +14,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col bg-cream dark:bg-ink">
+        {/* Facebook Meta Pixel */}
+        <MetaPixel />
+
+        {/* Site Header */}
         <Header />
 
+        {/* Main Content */}
         <main className="flex-1">
           {children}
         </main>
 
+        {/* Site Footer */}
         <Footer />
 
+        {/* Floating WhatsApp Button */}
         <WhatsAppButton />
       </body>
     </html>
